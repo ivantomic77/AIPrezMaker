49307282edef831db137a1e3b5c95eba96b98814
ivantomic77 <ivan.tomic2108@gmail.com>
2023-04-30 18:03:26 +0200

    creating presentation with title and text

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 import os
 import openai
 from dotenv import load_dotenv
+from pptx import Presentation
 import json
 
 load_dotenv()
 
-openai.organization = os.getenv('ORG')
-openai.api_key = os.getenv('API_KEY')
+def main():
+  openai.organization = os.getenv('ORG')
+  openai.api_key = os.getenv('API_KEY')
+  index = 0;
 
-prompt = 'Make me a complex presentation in language: Croatian. The required number of slides is: 2. The theme is: Prvi svjetski rat.  Presentation should not have a lot of text on one slide. Give me a presentation in this format strictly: { "Slide number": {"title": "Title", "text": "Slide text", "prompt": "Prompt I can feed to image generation" }, ...}'
-result = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=prompt,
-  max_tokens=1000,
-  temperature=1
-)
+  language = input("What language should the presentation use?\n")
+  theme = input("What is the topic for presentation?\n")
+  minSlides = input("How much slides should it contain?\n")
 
-print(json.loads(result.choices[0].text)["Slide 1"]["title"])
+  prompt = 'Make me a complex presentation in language: %s. The required number of slides is: %s. The theme is: %s.  Presentation should not have a lot of text on one slide. Give me a presentation in this format strictly: { "Slide number": {"title": "Title", "text": "Slide text", "prompt": "Prompt I can feed to image generation" }, ...}' % (language, minSlides,theme)
+  result = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    max_tokens=1000,
+    temperature=1
+  )
+  pptInJSON = json.loads(result.choices[0].text)
+
+  presentation = Presentation()
+  firstLayout = presentation.slide_layouts[0]  
+  mySlide = presentation.slides.add_slide(firstLayout)
+  myTitle = mySlide.shapes.title
+  mySubtitle = mySlide.shapes.placeholders[1]
+  myTitle.text = theme
+
+  for key in pptInJSON:
+    value = pptInJSON[key]
+    layout = presentation.slide_layouts[1]
+    slide = presentation.slides.add_slide(layout)
+    title = slide.shapes.title
+    title.text = value["title"]
+    body = slide.shapes.placeholders[1].text_frame
+    body.text = value["text"]
+  
+  presentation.save('myPPT.pptx')
+    
+
+if __name__ == '__main__':
+    main()
